6df10791c9ec44be382f69cf1f2c4b6cb35bbebb
veryheavypickle <xavier.carroll01@gmail.com>
2022-04-15 00:06:52 +0200

    Added more explanation, asserted list, added execute option

--- src/yodas/menuClass.py
+++ src/yodas/menuClass.py
@@ -2,34 +2,39 @@ class Menu:
     """
     This class takes a list of functions to put in a menu
 
-    To specify the function names, each item of the list must be a dictionary
-    in the format {"Quit python", exit}
+    items
+    in the format [{"Quit python", exit}]
+
+    execute
+    if true, the menu automatically executes the selected function
     """
-    def __init__(self, menuItems, title=None, subtitle=None):
-        self.menuItems = []
+    def __init__(self, items, title=None, subtitle=None, execute=True):
+        assert type(items) is list
+        self.items = []
         self.title = str(title)
-        self.subtitle = subtitle
-        for item in menuItems:
+        self.subtitle = str(subtitle)
+        self.execute = bool(execute)
+        for item in items:
             if callable(item):  # Is the menu item a function/callable variable?
                 try:
-                    self.menuItems.append({item.__name__: item})
+                    self.items.append({item.__name__: item})
                 except AttributeError:
                     # I will assume it is quit
-                    self.menuItems.append({"Quit": item})
+                    self.items.append({"Quit": item})
             elif type(item) is dict:
                 # Dictionary item must be in format
                 # {"String", functionVariable}
-                self.menuItems.append(item)
+                self.items.append(item)
 
-    def menu(self):
+    def show(self):
         print("=" * 100)
         if self.title:
             print("\n" + self.title)
         if self.subtitle:
             print("\n" + self.subtitle)
 
-        for i in range(len(self.menuItems)):
-            functionName = list(self.menuItems[i].keys())[0]
+        for i in range(len(self.items)):
+            functionName = list(self.items[i].keys())[0]
             print("{0} : {1}".format(i, functionName))
 
         # Select option
@@ -38,16 +43,19 @@ class Menu:
             option = int(input("\nChoose an option: "))
         except ValueError:
             print("Not a number :(")
-            self.menu()
+            self.show()
         except NameError:
             print("Not a number :(")
-            return self.menu()
+            return self.show()
 
         # Validate
-        if 0 <= option <= len(self.menuItems) - 1:
-            chosenFunctionName = list(self.menuItems[option].keys())[0]
-            chosenFunction = self.menuItems[option][chosenFunctionName]
+        if 0 <= option <= len(self.items) - 1:
+            chosenFunctionName = list(self.items[option].keys())[0]
+            chosenFunction = self.items[option][chosenFunctionName]
         else:
             print("Number out of bounds")
-            return self.menu()
+            return self.show()
+
+        if self.execute:
+            chosenFunction()
         return chosenFunction
